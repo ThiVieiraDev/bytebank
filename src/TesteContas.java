aa36f99aabeab9e1042d50b2e2453cd628db918c
Thi Vieira <thiiagoviieira@gmail.com>
2021-02-03 23:54:57 -0300

    Fim do módulo 4, Herança e uso dos construtores

--- src/TesteContas.java
+++ src/TesteContas.java
@@ -6,5 +6,8 @@ public class TesteContas {
         ContaPoupanca cp = new ContaPoupanca(222, 222);
         cp.deposita(200.0);
 
+        cc.transfere(10.0, cp);
+        System.out.println("CC: " + cc.getSaldo());
+        System.out.println("CP: " + cp.getSaldo());
     }
 }
