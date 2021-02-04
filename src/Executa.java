83618e8ede8f97a7a449ba5e59fe6ec8d7bebfc8
Thi Vieira <thiiagoviieira@gmail.com>
2021-02-04 00:17:01 -0300

    Fim do módulo 5, aplicado class e method abstract em todo o projeto

--- src/Executa.java
+++ src/Executa.java
@@ -1,8 +1,8 @@
-public class Executa {
-    public static void main(String[] args) {
-        Conta conta1 = new Conta(1010, 2321);
-        Cliente thiago = new Cliente();
-
-        conta1.setTitular(thiago);
-    }
-}
+//public class Executa {
+//    public static void main(String[] args) {
+//        Conta conta1 = new Conta(1010, 2321);
+//        Cliente thiago = new Cliente();
+//
+//        conta1.setTitular(thiago);
+//    }
+//}
